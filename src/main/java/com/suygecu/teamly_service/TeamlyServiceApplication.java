4ec334b24463a7fdbf843440971d24016d788783
Suygecu <Suygetcu@mail.ru>
2026-02-18 19:44:32 +0300

    fix

--- src/main/java/com/suygecu/teamly_service/TeamlyServiceApplication.java
+++ src/main/java/com/suygecu/teamly_service/TeamlyServiceApplication.java
@@ -4,10 +4,13 @@ import kotlin.jvm.Throws;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@ComponentScan(basePackages = {"com.suygecu.teamly_service", "controller", "service", "teamly"})
 @EnableScheduling
 @EnableAsync
 public class TeamlyServiceApplication implements CommandLineRunner {
